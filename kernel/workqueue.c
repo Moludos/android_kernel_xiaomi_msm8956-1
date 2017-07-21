901b6413f9a1becf23298052c274f85b91374a0e
psndna88 <psndna88@gmail.com>
2017-07-21 10:12:01 +0530

    workqueue: fix power efficient workqueue permissions
    # Conflicts:
    #	kernel/workqueue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -280,6 +280,15 @@ static cpumask_var_t *wq_numa_possible_cpumask;
 static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0444);
 
+/* see the comment above the definition of WQ_POWER_EFFICIENT */
+#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+static bool wq_power_efficient = true;
+#else
+static bool wq_power_efficient;
+#endif
+
+module_param_named(power_efficient, wq_power_efficient, bool, 0664);
+
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 
 /* buf for wq_update_unbound_numa_attrs(), protected by CPU hotplug exclusion */
